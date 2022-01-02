692c7151f1c2b84b908b38b14c90a503c17a8609
Kulyadev <al.kulya@angelic.com>
2022-01-02 13:07:16 +0300

    Remove 'Death Comes From On High' ac script, improve Lich King start script

--- sql/updates/world/2022_01_01_00_world_stormwind_Leria_Nightwind.sql
+++ sql/updates/world/2022_01_01_00_world_stormwind_Leria_Nightwind.sql
@@ -1,4 +1,5 @@
--- position, orientaion of Leria Nightwind
-UPDATE creature SET position_x='-8722.81',position_y='522.099',position_z='98.8999',orientation='1.84176' WHERE id = 61837;
--- update Leria Nightwind's posture to sitting  
-INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES ((SELECT guid FROM creature WHERE id= '61837'), 0, 0, 0, 257, 426, NULL);
+UPDATE `creature` SET `position_x` = -8722.81, `position_y` = 522.099, `position_z` = 98.8999, `orientation` = 1.84176 WHERE `id` = 61837;
+
+DELETE FROM `creature_addon` WHERE `guid` IN (SELECT `guid` FROM `creature` WHERE `id` = 61837);
+INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
+((SELECT `guid` FROM `creature` WHERE `id` = 61837), 0, 0, 0, 257, 426, NULL);
