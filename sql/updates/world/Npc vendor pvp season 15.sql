23440c5739e5dc169c83b35be8c87c93fab42f83
Ramys <limajosimar@yahoo.com.br>
2022-01-20 12:10:25 -0300

    Update Npc vendor season 15 Mop

---
+++ sql/updates/world/Npc vendor pvp season 15.sql
@@ -0,0 +1,8 @@
+UPDATE `world`.`creature_template` SET `npcflag`='129' WHERE  `entry`=73147;
+UPDATE `world`.`creature_template` SET `npcflag`='129' WHERE  `entry`=73146;
+UPDATE `world`.`creature_template` SET `npcflag`='129' WHERE  `entry`=73141;
+UPDATE `world`.`creature_template` SET `npcflag`='129' WHERE  `entry`=73142;
+UPDATE `world`.`creature_template` SET `npcflag`='129' WHERE  `entry`=73143;
+UPDATE `world`.`creature_template` SET `npcflag`='129' WHERE  `entry`=73144;
+UPDATE `world`.`creature_template` SET `npcflag`='129' WHERE  `entry`=73145;
+UPDATE `world`.`creature_template` SET `npcflag`='129' WHERE  `entry`=73148;
