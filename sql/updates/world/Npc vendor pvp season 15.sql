d894963c8c596171cf390b63677099507c5e7b56
Ramys <limajosimar@yahoo.com.br>
2022-02-06 12:49:17 -0300

    Delete Npc vendor pvp season 15.sql

--- sql/updates/world/Npc vendor pvp season 15.sql
+++
@@ -1,8 +0,0 @@
-UPDATE `world`.`creature_template` SET `npcflag`='129' WHERE  `entry`=73147;
-UPDATE `world`.`creature_template` SET `npcflag`='129' WHERE  `entry`=73146;
-UPDATE `world`.`creature_template` SET `npcflag`='129' WHERE  `entry`=73141;
-UPDATE `world`.`creature_template` SET `npcflag`='129' WHERE  `entry`=73142;
-UPDATE `world`.`creature_template` SET `npcflag`='129' WHERE  `entry`=73143;
-UPDATE `world`.`creature_template` SET `npcflag`='129' WHERE  `entry`=73144;
-UPDATE `world`.`creature_template` SET `npcflag`='129' WHERE  `entry`=73145;
-UPDATE `world`.`creature_template` SET `npcflag`='129' WHERE  `entry`=73148;
