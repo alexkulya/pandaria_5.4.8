62b644b2392c0b4b0cf85e522d8e8c349e433165
Kulyadev <al.kulya@angelic.com>
2022-03-09 21:02:42 +0300

    Added battlepay assortiment (basic for Pandaria 5.4.8)

---
+++ sql/updates/world/battlepay/battle_pay_group.sql
@@ -0,0 +1,16 @@
+TRUNCATE TABLE `battle_pay_group`;
+INSERT INTO `battle_pay_group` (`id`, `idx`, `name`, `icon`, `type`) VALUES
+(1, 1, "Featured", 939376, 1),
+(2, 2, "Mounts", 939379, 0),
+(3, 3, "Bags", 940857, 0),
+(4, 4, "Pets", 939380, 0),
+(5, 5, "Weapons", 940868, 0),
+(6, 6, "Armor", 940856, 0),
+(7, 7, "Food", 940861, 0),
+(8, 8, "Toys", 940867, 0),
+(9, 9, "Scroll", 940860, 0),
+(10, 10, "WoW", 940869, 0),
+(11, 11, "Services", 939382, 0),
+(12, 12, "Season 2", 939381, 0),
+(13, 13, "Mogushan-HM", 940856, 0),
+(14, 14, "Heart of Fear-NM", 939381, 0);
