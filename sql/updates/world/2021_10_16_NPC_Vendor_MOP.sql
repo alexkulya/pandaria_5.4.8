73ff1efb77d2a8e5c0b8c1c30d99b2a7d896c029
Terragor <81566364+zTerragor@users.noreply.github.com>
2022-04-19 11:56:22 +0500

    DB/QUEST ADD MOP VENDORS
    DB/QUEST ADD MOP VENDORS

---
+++ sql/updates/world/2021_10_16_NPC_Vendor_MOP.sql
@@ -0,0 +1,29 @@
+-- activa npc vendedores
+
+UPDATE `creature_template` SET `npcflag` = '4224' WHERE `entry` = '59908'; -- Jaluu el Generoso
+UPDATE `creature_template` SET `npcflag` = '4227' WHERE `entry` = '64001'; -- Sabia Flor de Loto
+UPDATE `creature_template` SET `npcflag` = '4227' WHERE `entry` = '64032'; -- Sabio Corazón Albo
+UPDATE `creature_template` SET `npcflag` = '4224' WHERE `entry` = '64595'; -- Rushi el Zorro
+UPDATE `creature_template` SET `npcflag` = '131' WHERE `entry` = '64599'; -- Herrero de ámbar Zikk
+UPDATE `creature_template` SET `npcflag` = '128' WHERE `entry` = '68363'; -- Sacudematas
+UPDATE `creature_template` SET `npcflag` = '4224' WHERE `entry` = '68364'; -- Paul North
+UPDATE `creature_template` SET `npcflag` = '4224' WHERE `entry` = '69059'; -- Agente Malley
+UPDATE `creature_template` SET `npcflag` = '4224' WHERE `entry` = '69060'; -- Destripacolmillo Grukna
+UPDATE `creature_template` SET `npcflag` = '129' WHERE `entry` = '65164'; -- Lok'nor Puñosangre
+UPDATE `creature_template` SET `npcflag` = '129' WHERE `entry` = '65165'; -- Doris Chiltonius
+UPDATE `creature_template` SET `npcflag` = '129' WHERE `entry` = '65166'; -- Hayden Christophen
+UPDATE `creature_template` SET `npcflag` = '129' WHERE `entry` = '65167'; -- Lucan Malory
+UPDATE `creature_template` SET `npcflag` = '129' WHERE `entry` = '65514'; -- Ethan Natice
+UPDATE `creature_template` SET `npcflag` = '129' WHERE `entry` = '65515'; -- Acon Penamuerte
+UPDATE `creature_template` SET `npcflag` = '128' WHERE `entry` = '71617'; -- Doris Chiltonius
+UPDATE `creature_template` SET `npcflag` = '128' WHERE `entry` = '71621'; -- Lok'nor Puñosangre
+UPDATE `creature_template` SET `npcflag` = '128' WHERE `entry` = '71623'; -- Lucan Malory
+UPDATE `creature_template` SET `npcflag` = '128' WHERE `entry` = '71624'; -- Hayden Christophen
+UPDATE `creature_template` SET `npcflag` = '129' WHERE `entry` = '73141'; -- Doris Chiltonius
+UPDATE `creature_template` SET `npcflag` = '129' WHERE `entry` = '73142'; -- Maestro de armas Holinka
+UPDATE `creature_template` SET `npcflag` = '129' WHERE `entry` = '73143'; -- Hayden Christophen
+UPDATE `creature_template` SET `npcflag` = '129' WHERE `entry` = '73144'; -- Lucan Malory
+UPDATE `creature_template` SET `npcflag` = '129' WHERE `entry` = '73145'; -- Acon Penamuerte
+UPDATE `creature_template` SET `npcflag` = '129' WHERE `entry` = '73146'; -- Lok'nor Puñosangre
+UPDATE `creature_template` SET `npcflag` = '129' WHERE `entry` = '73147'; -- Ethan Natice
+UPDATE `creature_template` SET `npcflag` = '128' WHERE `entry` = '73148'; -- Roo Desvin
