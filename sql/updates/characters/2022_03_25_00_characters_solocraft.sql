65e2f95ede1316ce5ba34b9a7870eab07dd792ef
Kulyadev <al.kulya@angelic.com>
2022-04-21 23:09:17 +0400

    Solocraft system update Thanks to LannyE & acidmanifesto

--- sql/updates/characters/2022_03_25_00_characters_solocraft.sql
+++ sql/updates/characters/2022_03_25_00_characters_solocraft.sql
@@ -1,8 +1,9 @@
+DROP TABLE IF EXISTS `custom_solocraft_character_stats`;
 CREATE TABLE IF NOT EXISTS `custom_solocraft_character_stats` (
-  `GUID` tinyint unsigned NOT NULL,
+  `GUID` tinyint(3) unsigned NOT NULL,
   `Difficulty` float NOT NULL,
-  `GroupSize` int NOT NULL,
-  `SpellPower` int unsigned NOT NULL DEFAULT "0",
+  `GroupSize` int(11) NOT NULL,
+  `SpellPower` int(10) unsigned NOT NULL DEFAULT "0",
   `Stats` float NOT NULL DEFAULT "100",
   PRIMARY KEY (`GUID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
