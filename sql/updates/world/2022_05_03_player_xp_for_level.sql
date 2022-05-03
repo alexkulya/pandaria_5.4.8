bd1ab3698e01802310ecd24bfb9fd6a535d136b5
Terragor <81566364+zTerragor@users.noreply.github.com>
2022-05-03 18:58:33 +0500

    DB/Fix Player XP for LVL corrections
    DB/Fix Player XP for LVL corrections

---
+++ sql/updates/world/2022_05_03_player_xp_for_level.sql
@@ -0,0 +1,10 @@
+UPDATE `player_xp_for_level` SET `xp_for_next_level`= 1074814  WHERE `lvl`= 75;
+UPDATE `player_xp_for_level` SET `xp_for_next_level`= 1085869  WHERE `lvl`= 76;
+UPDATE `player_xp_for_level` SET `xp_for_next_level`= 1097058  WHERE `lvl`= 77;
+UPDATE `player_xp_for_level` SET `xp_for_next_level`= 1108113  WHERE `lvl`= 78;
+UPDATE `player_xp_for_level` SET `xp_for_next_level`= 1119436  WHERE `lvl`= 79;
+UPDATE `player_xp_for_level` SET `xp_for_next_level`= 1020946  WHERE `lvl`= 70;
+UPDATE `player_xp_for_level` SET `xp_for_next_level`= 1031532  WHERE `lvl`= 71;
+UPDATE `player_xp_for_level` SET `xp_for_next_level`= 1042319  WHERE `lvl`= 72;
+UPDATE `player_xp_for_level` SET `xp_for_next_level`= 1053106  WHERE `lvl`= 73;
+UPDATE `player_xp_for_level` SET `xp_for_next_level`= 1063893  WHERE `lvl`= 74; 
