7b39a1a88793d5208bc416ea7b7a81cf63faf3d7
Terragor <81566364+zTerragor@users.noreply.github.com>
2022-06-29 09:42:44 +0300

    DB/FIX CREATURE_ADDON
    Agatha Flight Animation - 50372

---
+++ sql/updates/world/2022_06_29_agatha_50372.sql
@@ -0,0 +1,3 @@
+Agatha Flight Animation - 50372
+DELETE FROM `creature_template_addon` WHERE `entry`=50372;
+INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `aiAnimKit`, `movementAnimKit`, `meleeAnimKit`, `auras`) VALUES (50372, 0, 0, 50331648, 1, 0, 0, 0, 0, '');
