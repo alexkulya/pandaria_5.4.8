ac28a21293133819616cddf61a2d4bee4c8c46de
davidZX <david.gr79@gmail.com>
2022-08-11 23:12:22 -0600

    SQL/UPDATE +2022_08_12_00_fix_quest_31394

---
+++ sql/updates/world/2022_08_12_00_fix_quest_31394.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template` SET `KillCredit1` = 64528 AND `KillCredit2` = 64742 WHERE `entry` = 64744;
+UPDATE `quest_objective` SET `objectId` = 64742 WHERE `questId` = 31394 AND `index` = 3;
