113edb625cfa494f84b0f137f9525c08c475e0fd
Bonelesz <syrus2309@gmail.com>
2022-12-21 23:33:42 -1100

    Core: Fixed up damage of spell 69041 - "Rocket Barrage".

--- src/server/scripts/Spells/spell_generic.cpp
+++ src/server/scripts/Spells/spell_generic.cpp
@@ -4716,6 +4716,28 @@ class spell_gen_deserter : public SpellScript
     }
 };
 
+// 69041 - Rocket Barrage (Goblin Racial)
+class spell_gen_rocket_barrage : public SpellScript
+{
+    PrepareSpellScript(spell_gen_rocket_barrage);
+
+    void HandleDamage(SpellEffIndex /*effIndex*/)
+    {
+        if (Player* caster = GetCaster()->ToPlayer())
+        {
+            // Get highest attack power
+            uint32 ap = std::max(caster->GetTotalAttackPowerValue(BASE_ATTACK), caster->GetTotalAttackPowerValue(RANGED_ATTACK));
+
+            SetHitDamage(1 + (0.25f * ap) + (0.429f * caster->SpellBaseDamageBonusDone(SPELL_SCHOOL_MASK_FIRE)) + (caster->getLevel() * 2));
+        }
+    }
+
+    void Register()
+    {
+        OnEffectHitTarget += SpellEffectFn(spell_gen_rocket_barrage::HandleDamage, EFFECT_0, SPELL_EFFECT_SCHOOL_DAMAGE);
+    }
+};
+
 void AddSC_generic_spell_scripts()
 {
     new aura_script<spell_gen_absorb0_hitlimit1>("spell_gen_absorb0_hitlimit1");
@@ -4835,4 +4857,5 @@ void AddSC_generic_spell_scripts()
     new aura_script<spell_carrying_seaforium>("spell_carrying_seaforium");
     new aura_script<spell_gen_surgical_alterations>("spell_gen_surgical_alterations");
     new spell_script<spell_gen_deserter>("spell_gen_deserter");
+	new spell_script<spell_gen_rocket_barrage>("spell_gen_rocket_barrage");
 }
